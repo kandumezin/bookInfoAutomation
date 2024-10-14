70a1dda6f53463d20678b33e1a8baf7ef3cc8271
kandume <kanndumezinn@yahoo.co.jp>
2024-10-14 18:39:20 +0900

    pdf to code. you can import pdf to make ISBN and detailCode

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 from PIL import Image
+from pdf2image import convert_from_path
+import tempfile
 import pyzbar.pyzbar as pyzbar
+import os
 
-im = Image.open("goodCodes.jpg")
 
-def readCode(image):
+
+def readCode(pdf: str, page_num: int) -> dict:
     """
-    関数の概要
-    あたえられた画像データから書籍JANコードを取り出し、ISBNとdetailCodeが内包された辞書型にして返します。
-
-    書籍JANコード は、２段のバーコードのセットで構成されています。
+    指定されたpathにあるpdfファイル（引数: pdf）の、指定された一つのページ（引数: page_num）から、
+    書籍JANコードを取り出し、ISBNとdetailCodeが内包された辞書型（{ISBN: , detailCode:}）にして返します。
+    
+    書籍JANコード は、２つのバーコードのセットで構成されています。
     一段目には、ISBNが内包されています。978から始まる整数の列です。
     二段目には、図書分類コードおよび、図書本体価格が内包されています（便宜上、"detailCode"と表記します）。192から始まる整数の列です。
     接頭辞である978と192を用いて、ISBNとdetailCodeを見分けています。
     """
-    
-    # 画像データからバーコードを読み込みます
-    pre_bcodes = pyzbar.decode(image)
+
+    # 指定されたpdfページの画像ファイルを作成します
+    im = convert_from_path(pdf, first_page=page_num, last_page=page_num)
+    # 指定されたページの画像は、一時的なファイルとして保存されます
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_f:
+        im[0].save(temp_f.name, format="PNG")
+        temp_file_path = temp_f.name
+        
+    # 一時的な画像ファイルからバーコードを読み込みます
+    try:
+        with Image.open(temp_file_path) as img:
+            pre_bcodes = pyzbar.decode(img)
+    # バーコードを読み込んだら、一時的なファイルを削除します
+    finally:
+        os.remove(temp_file_path)
 
     # 画像データ内に、書籍JANコードがあるか調べます
     # まずは、数で判定します
@@ -36,4 +51,8 @@ def readCode(image):
 
     return bookJAN
 
-print(readCode(im))
+
+
+if __name__ == "__main__":
+    pf = "test-comic.pdf"
+    print(readCode(pf, 3))
--- test.py
+++ test.py
@@ -1,8 +1,37 @@
 from PIL import Image
+from pdf2image import convert_from_path
+import tempfile
 import pyzbar.pyzbar as pyzbar
+import os
 
-im = Image.open("tooMuchCodes.jpg")
+pd = "test-comic.pdf"
 
-pre_bcodes = pyzbar.decode(im)
+def readCode(pdf: str, whereIs: int) -> dict:
+    """
+    関数の概要
+    あたえられたpathにあるpdfから、書籍JANコードを取り出し、ISBNとdetailCodeが内包された辞書型にして返します。
+    注意点として、指定された1つのページから書籍JANコードを読み込むので、事前に、どのページに書籍JANコードがあるか知っておく必要があります。
 
-print(pre_bcodes)
+    書籍JANコード は、２段のバーコードのセットで構成されています。
+    一段目には、ISBNが内包されています。978から始まる整数の列です。
+    二段目には、図書分類コードおよび、図書本体価格が内包されています（便宜上、"detailCode"と表記します）。192から始まる整数の列です。
+    
+    接頭辞である978と192を用いて、ISBNとdetailCodeを見分けています。
+    """
+
+    # 指定されたページ（書籍JANコードがあるとされているページ）の画像データを作成します。
+    im = convert_from_path(pdf, first_page=whereIs, last_page=whereIs)
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_f:
+        im[0].save(temp_f.name, format="PNG")
+        temp_file_path = temp_f.name
+        
+    # 画像データからバーコードを読み込みます
+    try:
+        with Image.open(temp_file_path) as im:
+            pre_bcodes = pyzbar.decode(im)
+    finally:
+        os.remove(temp_file_path)
+    return pre_bcodes
+
+print(readCode(pd, 3))
